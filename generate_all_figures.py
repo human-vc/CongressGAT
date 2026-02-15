d8e9c17cd908806b5250b5fd1f247fd1abf4fbdd
Jacob Crainic <jacobcrainic@MacBook-Air-152.hsd1.fl.comcast.net>
2026-02-15 14:07:44 -0500

    Nudge cross-party text to right edge (x=1.0)

--- generate_all_figures.py
+++ generate_all_figures.py
@@ -117,7 +117,7 @@ def draw_network_panel(ax, congress, title):
     ax.legend(loc='upper left', frameon=True, framealpha=0.9)
     ax.set_xlim(-1.1, 1.1)
     n_cross = len([1 for i, j in [(i,j) for i in range(n) for j in range(i+1,n) if adj[i,j]>0] if parties[i]!=parties[j]]) if n < 100 else len(cross_edges)
-    ax.text(0.99, 0.03, f'Cross-party edges: {len(cross_edges)}', transform=ax.transAxes,
+    ax.text(1.0, 0.04, f'Cross-party edges: {len(cross_edges)}', transform=ax.transAxes,
            ha='right', va='bottom', fontsize=8, fontweight='bold', color='black')
 
 
